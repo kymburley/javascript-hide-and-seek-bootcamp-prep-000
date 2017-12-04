e10fa931cf244e2c7b1f6e261f1fb662cf0c3278
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-04 14:24:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+
+  for ( let i in lis ) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+
+  let current = document.getElementById('grand-node').querySelectorAll('div');
+  let next = [];
+
+  while (current) {
+    /*if (criteriaFn(current)) {
+      return current;
+    }*/
+
+   	if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i]);
+      }
+    }
+
+    current = next.shift();
+  }
+
+  return current.innerHTML;
+}
