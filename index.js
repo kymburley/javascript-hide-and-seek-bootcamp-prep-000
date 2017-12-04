4293063067181111759521759eba644bf3fe3396
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-04 15:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function deepestChild() {
     }
 
    	if (Array.isArray(current)) {
-      for (let i in current) {
+      for (let i = 0; i < current.length; i++) {
         next.push(current[i]);
       }
     }
